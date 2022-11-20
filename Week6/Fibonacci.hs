3e039e337041e59cfc5d93bd91b47d45066a950b
chanhpar <chanhpar@student.42seoul.kr>
2022-11-21 00:49:29 +0900

    Week6 HW done

--- Week6/Fibonacci.hs
+++ Week6/Fibonacci.hs
@@ -2,9 +2,9 @@
 
 -- Exercise 1
 fib :: Integer -> Integer
-fib x
-  | x <= 1 = x
-  | otherwise = fib (x - 1) + fib (x - 2)
+fib a
+  | a <= 1 = a
+  | otherwise = fib (a - 1) + fib (a - 2)
 
 fibs1 :: [Integer]
 fibs1 = map fib [0..]
@@ -12,34 +12,71 @@ fibs1 = map fib [0..]
 -- Exercise 2
 fibs2 :: [Integer]
 fibs2 = fibs2' 0 1
-  where fibs2' x y = x : fibs2' y (x + y)
+  where fibs2' a b = a : fibs2' b (a + b)
 
 -- Exercise 3
 data Stream a = Cons a (Stream a)
 
 streamToList :: Stream a -> [a]
-streamToList (Cons x y) = x : streamToList y
+streamToList (Cons a b) = a : streamToList b
 
 instance Show a => Show (Stream a) where
-  show x = show (take 30 (streamToList x))
+  show a = show (take 30 (streamToList a))
 
 -- Exercise 4
 streamRepeat :: a -> Stream a
-streamRepeat = \x -> Cons x (streamRepeat x)
+streamRepeat = \a -> Cons a (streamRepeat a)
 
 streamMap :: (a -> b) -> Stream a -> Stream b
-streamMap fn = \(Cons x y) -> Cons (fn x) (streamMap fn y)
+streamMap fn = \(Cons a b) -> Cons (fn a) (streamMap fn b)
 
 streamFromSeed :: (a -> a) -> a -> Stream a
-streamFromSeed fn = \x -> Cons x (streamFromSeed fn (fn x))
+streamFromSeed fn = \a -> Cons a (streamFromSeed fn (fn a))
 
 -- Exercise 5
 nats :: Stream Integer
 nats = streamFromSeed (+1) 0
 
 interleaveStreams :: Stream a -> Stream a -> Stream a
-interleaveStreams (Cons x y) z = Cons x (interleaveStreams z y)
+interleaveStreams (Cons a b) c = Cons a (interleaveStreams c b)
 
 ruler :: Stream Integer
 ruler = fn 0
-  where fn x = interleaveStreams (streamRepeat x) (fn (x + 1))
+  where fn a = interleaveStreams (streamRepeat a) (fn (a + 1))
+
+-- Exercise 6
+x :: Stream Integer
+x = Cons 0 (Cons 1 (streamRepeat 0))
+
+instance Num (Stream Integer) where
+  fromInteger = streamFromSeed (*0)
+  negate = streamMap (negate)
+  (+) (Cons a b) (Cons c d) = Cons (a + c) (b + d)
+  (*) (Cons a b) (Cons c d) = Cons (a * c) (streamMap (* a) d + (b * (Cons c d)))
+  abs = streamMap abs
+  signum = streamMap signum
+
+division :: (Stream Integer) -> (Stream Integer) -> (Stream Integer)
+division (Cons a b) (Cons c d) = q
+  where q = Cons (div a c) (streamMap (`div` c) (b - q * d))
+
+fibs3 :: Stream Integer
+fibs3 = division x (1 - x - x * x)
+
+-- Exercise 7
+data Matrix a = Matrix a a a a
+
+instance Num (Matrix Integer) where
+  fromInteger n = Matrix n n n n
+  negate (Matrix a b c d) = Matrix (negate a) (negate b) (negate c) (negate d)
+  abs (Matrix a b c d) = Matrix (abs a) (abs b) (abs c) (abs d)
+  signum (Matrix a b c d) = Matrix (signum a) (signum b) (signum c) (signum d)
+  (+) (Matrix a b c d) (Matrix a' b' c' d') = Matrix (a + a') (b + b') (c + c') (d + d')
+  (*) (Matrix a b c d) (Matrix a' b' c' d') = Matrix (a * a' + b * c') (a * b' + b * d') (c * a' + d * c') (c * b' + d * d')
+
+fiboMat :: Matrix Integer
+fiboMat = Matrix 1 1 1 0
+
+fibs4 :: Integer -> Integer
+fibs4 0 = 0
+fibs4 n = (\(Matrix _ b _ _) -> b) (fiboMat ^ n)
