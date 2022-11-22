4a4ecf427dd116dd481f8ef0b750440d6907ff89
chanha-park <chanhpar@student.42seoul.kr>
2022-11-22 17:13:43 +0900

    solve HW7

--- Week7/JoinList.hs
+++ Week7/JoinList.hs
@@ -1,9 +1,14 @@
 {-# OPTIONS_GHC -Wall -Werror -Wextra #-}
+{-# LANGUAGE FlexibleInstances    #-}
+{-# LANGUAGE TypeSynonymInstances #-}
 
 module JoinList where
 
+import           Buffer
+import           Scrabble
 import           Sized
 
+-- Simplified example
 -- data JoinListBasic a = Empty
 --                      | Single a
 --                      | Append (JoinListBasic a) (JoinListBasic a)
@@ -17,6 +22,20 @@ data JoinList m a = Empty
                   | Single m a
                   | Append m (JoinList m a) (JoinList m a)
                   deriving (Eq, Show)
+
+
+-- newtype Product a = Product a
+--     deriving (Eq, Ord, Num, Show)
+
+-- getProduct :: Product a -> a
+-- getProduct (Product a) = a
+
+-- instance Num a => Monoid (Product a) where
+--   mempty = Product 1
+--   mappend = (<>)
+
+-- instance Num a => Semigroup (Product a) where
+--   (<>) = (*)
 
 -- Exercise 1
 (+++) :: Monoid m => JoinList m a -> JoinList m a -> JoinList m a
@@ -33,19 +52,57 @@ indexJ n _ | n < 0 = Nothing
 indexJ _ Empty = Nothing
 indexJ 0 (Single _ x) = Just x
 indexJ _ (Single _ _) = Nothing
-indexJ n (Append x y z)
-  | n < getSize (size x) = indexJ n y
-  | otherwise = indexJ (n - getSize ( size x)) z
+indexJ n (Append _ x y)
+  | n < sizeX = indexJ n x
+  | otherwise = indexJ (n - sizeX) y
+  where sizeX = (getSize . size . tag) x
+
 -- indexJ n x = (!!?) (jlToList x) n
 
-
-(!!?) :: [a] -> Int -> Maybe a
-(!!?) [] _ = Nothing
-(!!?) _ n | n < 0 = Nothing
-(!!?) (x : _) 0 = Just x
-(!!?) (_ : xs) n = (!!?) xs (n - 1)
+-- (!!?) :: [a] -> Int -> Maybe a
+-- (!!?) [] _ = Nothing
+-- (!!?) _ n | n < 0 = Nothing
+-- (!!?) (x : _) 0 = Just x
+-- (!!?) (_ : xs) n = (!!?) xs (n - 1)
 
 jlToList :: JoinList m a -> [a]
 jlToList Empty            = []
 jlToList (Single _ a)     = [a]
 jlToList (Append _ l1 l2) = jlToList l1 ++ jlToList l2
+
+dropJ :: (Sized b, Monoid b) => Int -> JoinList b a -> JoinList b a
+dropJ n x | n < 0 = x
+dropJ _ Empty = Empty
+dropJ 0 (Single m x) = Single m x
+dropJ _ (Single _ _) = Empty
+dropJ n (Append _ x y)
+  | n < sizeX = (dropJ n x) +++ y
+  | otherwise = dropJ (n - sizeX) y
+  where sizeX = (getSize . size . tag) x
+
+takeJ :: (Sized b, Monoid b) => Int -> JoinList b a -> JoinList b a
+takeJ n _ | n < 0 = Empty
+takeJ _ Empty = Empty
+takeJ 0 (Single _ _) = Empty
+takeJ _ (Single m x) = Single m x
+takeJ n (Append _ x y)
+  | n < sizeX = takeJ n x
+  | otherwise = x +++ takeJ (n - sizeX) y
+  where sizeX = (getSize . size . tag) x
+
+instance Monoid m => Monoid (JoinList m a) where
+  mempty = Empty
+
+instance Monoid m => Semigroup (JoinList m a) where
+  (<>) = (+++)
+
+scoreLine :: String -> JoinList Score String
+scoreLine = \x -> Single (scoreString x) x
+
+instance Buffer (JoinList (Score, Size) String) where
+  toString = unlines . jlToList
+  fromString = mconcat . fmap (\x -> Single (scoreString x, Size 1) x) . lines
+  line = indexJ
+  numLines = getSize . snd . tag
+  value = getScore . fst . tag
+  replaceLine n str x = takeJ n x +++ fromString str +++ dropJ (n + 1) x
--- Week7/Scrabble.hs
+++ Week7/Scrabble.hs
@@ -1,3 +1,43 @@
 {-# OPTIONS_GHC -Wall -Werror -Wextra #-}
+{-# LANGUAGE FlexibleInstances    #-}
+{-# LANGUAGE TypeSynonymInstances #-}
 
 module Scrabble where
+
+import Data.Char
+
+-- Exercise 3
+newtype Score = Score Int
+    deriving (Eq, Ord, Show, Num)
+
+getScore :: Score -> Int
+getScore (Score i) = i
+
+class Scored a where
+  scored :: a -> Score
+
+instance Scored Score where
+  scored = id
+
+instance Scored a => Scored (a, b) where
+  scored = scored . fst
+
+instance Semigroup Score where
+  (<>) = (+)
+
+instance Monoid Score where
+  mempty = Score 0
+
+score :: Char -> Score
+score x
+  | (elem . toLower) x "aeilnorstu" = Score 1
+  | (elem . toLower) x "dg" = Score 2
+  | (elem . toLower) x "bcmp" = Score 3
+  | (elem . toLower) x "hvwy" = Score 4
+  | (elem . toLower) x "k" = Score 5
+  | (elem . toLower) x "jx" = Score 8
+  | (elem . toLower) x "qz" = Score 10
+  | otherwise = Score 0
+
+scoreString :: String -> Score
+scoreString = mconcat . fmap score
