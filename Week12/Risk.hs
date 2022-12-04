3b35fb5a14744dde62d739a888ba1ee2f06f3073
chanhpar <chanhpar@student.42seoul.kr>
2022-12-05 02:36:07 +0900

    WIP HW12: exercise 2, 4, 5

--- Week12/Risk.hs
+++ Week12/Risk.hs
@@ -32,9 +32,24 @@ battle :: Battlefield -> Rand StdGen Battlefield
 battle (Battlefield x y) = pure (Battlefield x' y')
   where
     (x', y') = fight (x, y)
+-- battle (Battlefield x y) = throw x >>= \xs ->
+--     throw y >>= \ys ->
+--     (unDV . (evalRandIO . (fmap maximum xs))) >>= \valueX ->
+--     unDV <$> (evalRandIO . (fmap maximum) $ ys) >>= \valueY ->
+--     return (Battlefield (fst . fight $ (valueX, valueY)) (snd . fight $ (valueX, valueY)))
 
 fight :: (Army, Army) -> (Army, Army)
-fight = undefined
+fight (x, y)
+  | x > y = (x, y - 1)
+  | otherwise = (x - 1, y)
+
+--   where
+--     valueX <- evalRandIO . (fmap maximum) . (replicateM x) $ die
+--     valueX <- evalRandIO . (fmap maximum) . (replicateM x) $ die
+
+throw n = unDV <$> (evalRandIO . (fmap maximum) . (replicateM n) $ die)
+-- throw :: Int -> Rand StdGen [DieValue]
+-- throw n = replicateM n die
 
 -- Exercise 3
 invade :: Battlefield -> Rand StdGen Battlefield
@@ -52,5 +67,14 @@ numOfInvasion = 1000
 -- Exercise 5
 exactSuccessProb :: Battlefield -> Double
 exactSuccessProb = undefined
+
+-- main :: IO ()
+-- main = do
+--     let a = replicateM 3 die
+--     let b = fmap maximum a
+--     let c = evalRandIO b
+--     d <- c
+--     putStrLn (show d)
+--     return ()
 
 -- evalRandIO :: Rand StdGen a -> IO a
