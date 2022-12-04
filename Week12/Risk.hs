b147c1755b9b5fba45820ef92d46aa68a0ef12b4
chanhpar <chanhpar@student.42seoul.kr>
2022-12-05 01:01:27 +0900

    WIP: HW12

--- Week12/Risk.hs
+++ Week12/Risk.hs
@@ -7,15 +7,15 @@ import Control.Monad.Random
 ------------------------------------------------------------
 -- Die values
 
-newtype DieValue = DV { unDV :: Int } 
+newtype DieValue = DV {unDV :: Int}
   deriving (Eq, Ord, Show, Num)
 
 first :: (a -> b) -> (a, c) -> (b, c)
 first f (a, c) = (f a, c)
 
 instance Random DieValue where
-  random           = first DV . randomR (1,6)
-  randomR (low,hi) = first DV . randomR (max 1 (unDV low), min 6 (unDV hi))
+  random = first DV . randomR (1, 6)
+  randomR (low, hi) = first DV . randomR (max 1 (unDV low), min 6 (unDV hi))
 
 die :: Rand StdGen DieValue
 die = getRandom
@@ -23,6 +23,34 @@ die = getRandom
 ------------------------------------------------------------
 -- Risk
 
+-- Exercise 2
 type Army = Int
 
-data Battlefield = Battlefield { attackers :: Army, defenders :: Army }
+data Battlefield = Battlefield {attackers :: Army, defenders :: Army}
+
+battle :: Battlefield -> Rand StdGen Battlefield
+battle (Battlefield x y) = pure (Battlefield x' y')
+  where
+    (x', y') = fight (x, y)
+
+fight :: (Army, Army) -> (Army, Army)
+fight = undefined
+
+-- Exercise 3
+invade :: Battlefield -> Rand StdGen Battlefield
+invade x
+  | attackers x < 2 = pure x
+  | otherwise = battle x >>= invade
+
+-- Exercise 4
+successProb :: Battlefield -> Rand StdGen Double
+successProb = undefined
+
+numOfInvasion :: Int
+numOfInvasion = 1000
+
+-- Exercise 5
+exactSuccessProb :: Battlefield -> Double
+exactSuccessProb = undefined
+
+-- evalRandIO :: Rand StdGen a -> IO a
